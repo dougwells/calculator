4283d5a8810c58e3dc4b035fd015d9041434f43f
dougwells <dougwells@gmail.com>
2016-01-25 08:33:49 -0700

    Exploring forms for calculator functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,39 +1,25 @@
-$(document).ready(function() {
+$(function() {
 
-  //Download new quote & image URL from json datafile stored in my dropbox
-  //Replaces existing quote and image on webpage
-  $("#newQuote").click(function(){
-    $.ajax({
-      url: 'https://dl.dropboxusercontent.com/s/p5zx9nv7qpyexnc/quotes.json',
-      dataType: 'json',
-      success: function(data) {
-        var i = Math.floor(Math.random() * (data.length));
-        var quote = data[i].quote;
-        var author = data[i].author;
-        var imageUrl = data[i].imageUrl;
-        var imageColor = data[i].imageColor;
-        var twitterPost = "Love this quote by "+author+": '"+quote+"'";
-        var twitterPost140 = twitterPost.length > 140 ? twitterPost.substring(0,137)+"..." : twitterPost;
-        var quoteURI = encodeURI(twitterPost140);
-        var twitterQueryString = "https://twitter.com/intent/tweet?text="+quoteURI;
-        $('#quote').text("'"+quote+"'");
-        $('#quote-image').attr("src", imageUrl);
-        $('#author').text('Author: '+author);
-        $("#shareTwitter").attr("href", twitterQueryString);
-        console.log(twitterPost);
-        console.log(twitterQueryString);
-        console.log(twitterPost140);
-        console.log(twitterPost140.length);
-      },
-      error: function(err) {
-        var quote = "This app is shit!  You have an error. Error Message: "+ err;
-        var author = "Steve Jobs"
-        var imageUrl = "https://dl.dropboxusercontent.com/s/5ris4qilrskw5mr/sucks.jpg"
-        $("#quote").text("'"+quote+"'");
-        $('#quote-image').attr("src", imageUrl);
-        $('#author').text('Author: '+author);
-        console.log(quote);
+  $(".numbers-row").append('<div class="inc button">+</div><div class="dec button">-</div>');
+
+  $(".button").on("click", function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+
+    if ($button.text() == "+") {
+  	  var newVal = parseFloat(oldValue) + 1;
+  	} else {
+	   // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+	    } else {
+        newVal = 0;
       }
-    });
+	  }
+
+    $button.parent().find("input").val(newVal);
+
   });
+
 });
